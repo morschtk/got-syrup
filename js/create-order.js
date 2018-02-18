c322a8bf005780fbf923939f5ef63692f4e09501
Tyler Morsch <tmorsch@trovedata.com>
2018-02-18 00:27:10 -0500

    Created confirm and thank you pages added city attr, updated styles, changed image names, need to remove inline css for confirm and thankyou page.

--- js/create-order.js
+++ js/create-order.js
@@ -1,4 +1,15 @@
 "use strict"; // Start of use strict
+$(checkTotal);
+
+function checkTotal() {
+  $('#total').text((+$('#12ozQuantity').val()*9) + (+$('#32ozQuantity').val()*18) + (+$('#64ozQuantity').val()*30) + (+$('#128ozQuantity').val()*54));
+}
+
+function editForm() {
+  $('.order').show();
+  $('.confirmation').hide();
+  $('.thank-you').hide();
+}
 
 function addItem(item) {
   var str = '#'+item+'Quantity';
@@ -47,7 +58,7 @@ function removeItem(item) {
 }
 
 $('.quantity').change(function() {
-  $('#total').text((+$('#12ozQuantity').val()*9) + (+$('#32ozQuantity').val()*18) + (+$('#64ozQuantity').val()*30) + (+$('#128ozQuantity').val()*54));
+  checkTotal();
 });
 
 var stripe = Stripe('');
@@ -134,6 +145,7 @@ cardCvc.addEventListener('change', function(event) {
   }
 });
 
+var tok;
 var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
@@ -143,10 +155,46 @@ form.addEventListener('submit', function(event) {
     //   var errorElement = document.getElementById('card-errors');
     //   errorElement.textContent = result.error.message;
     // } else {
-      stripeTokenHandler(result.token);
+      tok = result.token.id;
+      $('.order').hide();
+      $('.confirmation').show();
+      $('.thank-you').hide();
+
+      $('#confirm-name').text($('#payment-form input[name=first_name]').val() + ' ' + $('#payment-form input[name=last_name]').val());
+      $('#confirm-addr1').text($('#payment-form input[name=address_1]').val());
+      $('#confirm-addr2').text($('#payment-form input[name=address_2]').val());
+      $('#confirm-state').text($('#payment-form input[name=city]').val() + ', ' + $('#payment-form select[name=state]').val() + ' ' + $('#payment-form input[name=zip]').val());
+      $('#confirm-email').text($('#payment-form input[name=email]').val());
+
+      $('#confirm-card').text(result.token.card.brand + ' ending in ' + result.token.card.last4);
+
+      var quantity = $('.quantity');
+      var quantityHtml = '';
+      quantity.each(function () {
+        if ($(this).val().length > 0) {
+          quantityHtml = quantityHtml + `<div class="item" style="display: inline-flex; align-items: center; margin-bottom: 10px; background-color: #ddd;">
+          <img class="ui avatar image small" src="img/` + $(this).attr('name') + `.jpg">
+          <div class="content" style="margin-left: 15px">
+          <a class="header">` + $(this).attr('data-title') + ' - ' + $(this).attr('data-size') + `</a>
+          <div class="description">$` + $(this).attr('data-price') + `</div>
+          <div class="description">Quantity: ` + $(this).val() + `</div>
+          </div>
+          </div>`;
+        }
+      });
+
+      $('#confirm-quantity').html(quantityHtml);
+
+      $('#confirm-total').text($('#total').text());
     }
   });
 });
+
+function confirmPurchase() {
+    if (tok !== null) {
+      stripeTokenHandler(tok);
+    }
+}
 
 function stripeTokenHandler(token) {
   // Insert the token ID into the form so it gets submitted to the server
@@ -154,7 +202,7 @@ function stripeTokenHandler(token) {
   var hiddenInput = document.createElement('input');
   hiddenInput.setAttribute('type', 'hidden');
   hiddenInput.setAttribute('name', 'stripeToken');
-  hiddenInput.setAttribute('value', token.id);
+  hiddenInput.setAttribute('value', token);
   form.append(hiddenInput);
   var totalInput = document.createElement('input');
   totalInput.setAttribute('type', 'hidden');
@@ -164,8 +212,16 @@ function stripeTokenHandler(token) {
 
   // Submit the form
   $.post('', form.serialize(), function(cred) {
-    console.log(cred);
+    thankCustomer();
   }).fail(function(err) {
     console.log(err);
   });
+}
+
+function thankCustomer() {
+  $('.order').hide();
+  $('.confirmation').hide();
+  $('.thank-you').show();
+  var thankText = `<span>Thank you so much ` + $('#confirm-name').text() + `! We apprciate your business and your love for locally grown quality syrup.</span><span>You will receive an email at ` + $('#confirm-email').text() + ` with more information about your order!</span>`;
+  $('.thank-you .message').html(thankText);
 }
